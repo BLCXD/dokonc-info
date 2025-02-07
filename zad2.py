bb4bbe5f93ee2b6deb7d8771a127b0b5f064cc69
BLCXD <blctoblc223@gmail.com>
2025-02-07 20:37:18 +0100

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   zad2.py

--- zad2.py
+++ zad2.py
@@ -1,36 +1,37 @@
 print("Witaj w programie do tworzenia Listy Zakupów Weekendowych!")
 produkty = []
 ilosc = []
+zakupy = {}
 
+# Funkcja do sortowania
+def sort(element):
+    produkt, ilosc = element
+    return -ilosc, produkt  
 
 while True:
     item = input("Podaj produkt lub wpisz 'koniec', aby zakończyć dodawanie produktów: ")
 
+
     #print(produkty)
     if item == 'koniec':
-        '''
-        print("twoja lista")
-        for i in range(len(produkty)):
 
-            print(f'{produkty[i]} - {ilosc[i]}szt.')
+        
+        posortowane = sorted(zakupy.items(), key=sort)
+
+        
+        i = 1
+        for produkt, ilosc in posortowane:
+            print(str(i) + ". " + produkt + " - " + str(ilosc) + "szt.")
+            i += 1
         break
-        '''
-        produkty_s = {}
-        for i in range(len(produkty)):
-
-            produkty_s[produkty[i]] = ilosc[i]
-            x = dict(sorted((produkty_s.items())))
-
-
-
-        print(produkty_s)
-        print(x)
-        break
-
     else:
-        produkty.append(item)
-        count = int(input("Podaj ilość: "))
-        ilosc.append(count)
+        try:
+            count = int(input("Podaj ilość: "))
+            zakupy[item] = count
+        except ValueError:
+            print("Błąd")
+        
+
 
 
 
